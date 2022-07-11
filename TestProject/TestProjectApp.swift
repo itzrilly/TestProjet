f9dc7e6bb388580085d9830275714af963f68ee9
itzrilly <rilwanougarbarilly@gmail.com>
2022-07-11 18:02:43 +0100

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by RILLY on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
