ac6f708a648f913d88dbd7809daf649cb45deab6
itzrilly <rilwanougarbarilly@gmail.com>
2022-07-28 10:00:53 +0100

    Basics of SwiftUI

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let names = ["Name 1", "Name 2", "Name 3", "Name 4", "Name 5"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Rectangle()
+                .foregroundColor(.red)
+                .tabItem {
+                    Label {
+                        Text("Home")
+                    } icon: {
+                        Image(systemName: "house")
+                    }
+
+                }
+            Rectangle()
+                .foregroundColor(.green)
+                .tabItem {
+                    Label {
+                        Text("Favourites")
+                    } icon: {
+                        Image(systemName: "heart")
+                    }
+
+                }
+            Rectangle()
+                .foregroundColor(.yellow)
+                .tabItem {
+                    Label {
+                        Text("Messages")
+                    } icon: {
+                        Image(systemName: "paperplane")
+                    }
+
+                }
+        }
     }
 }
 
